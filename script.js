cadc4ca69e60c0123ab8ce63f3fb2a1c77fb6f25
michellemnguyen <michellemnguyen@ucsb.edu>
2020-04-20 21:39:58 -0700

    Flex for modal fix

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function topFunction() {
 
 // Open the Modal
 function openModal() {
-    document.getElementById("myModal").style.display = "inline-block";
+    document.getElementById("myModal").style.display = "flex";
 }
   
 // Close the Modal
@@ -64,7 +64,7 @@ function showSlides(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.display = "flex";
     dots[slideIndex-1].className += " active";
     captionText.innerHTML = dots[slideIndex-1].alt;
 }
